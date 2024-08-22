2722595fe0063330fed8baedbd02b8d3fed1fb3f
Developer2 <gcpprep2022@gmail.com>
2024-08-22 18:23:43 +0530

    Updated .gitignore

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO employees (first_name, last_name, email_id) VALUES ('John', 'Doe', 'john.doe@example.com');
-INSERT INTO employees (first_name, last_name, email_id) VALUES ('Jane', 'Smith', 'jane.smith@example.com');
-INSERT INTO employees (first_name, last_name, email_id) VALUES ('Alice', 'Johnson', 'alice.johnson@example.com');
